fc77c0460dd3df15ebc9bd7b803448c435a70ea5
Engin Marshall <engin.marshall@gmail.com>
2024-11-19 16:51:01 +0100

    Working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,66 @@
-import { useState } from "react";
-import { AddTodo } from "./components/AddTodo/AddTodo";
-import { EditTodo } from "./components/EditTodo/EditTodo";
-import { FilterTodo } from "./components/FilterTodo/FilterTodo";
-import { TodoList } from "./components/TodoList/TodoList";
+import React, { useState } from "react";
+
+import { useSelector } from "react-redux";
+
+import type { RootState } from "./redux/store";
+
+import AddTodo from "./components/AddTodo/AddTodo";
+
+import EditTodo from "./components/EditTodo/EditTodo";
+
+import FilterTodo from "./components/FilterTodo/FilterTodo";
+
+import TodoList from "./components/TodoList/TodoList";
+
+
 
 export interface TodoInterface {
+
   id: string;
+
   task: string;
+
   completed: boolean;
+
 }
+
+
 
 const App = () => {
 
-  const { editTodo, setEditTodo } = useState<TodoInterface | null>(null);
+  const todos = useSelector((state: RootState) => state.todos.todos);
+  const [editTodo, setEditTodo] = useState<TodoInterface | null>(null);
+  const [todoFilterValue, setTodoFilterValue] = useState("all");
+
+  const getTodoFilterValue = (filterValue: string) =>
+    setTodoFilterValue(filterValue);
+  const getEditTodo = (editTodo: TodoInterface) => setEditTodo(editTodo);
 
   return (
     <main className="app">
       <div className="app__wrapper">
         <div className="app__header">
-          <h1 className="app__title">
-
-          </h1>
+          <h1 className="app__title">Todo App</h1>
         </div>
-
         <div className="app__inputs-box">
-          {editTodo?.id ? <EditTodo /> : <AddTodo />}
-          <FilterTodo />
+          {editTodo?.id ? (
+            <EditTodo editTodo={editTodo} setEditTodo={setEditTodo} />
+          ) : (
+            <AddTodo />
+          )}
+          <FilterTodo getTodoFilterValue={getTodoFilterValue} />
         </div>
-        <TodoList />
+        <TodoList
+          todos={todos}
+          todoFilterValue={todoFilterValue}
+          getEditTodo={getEditTodo}
+          setEditTodo={setEditTodo}
+          editTodo={editTodo}
+        />
       </div>
     </main>
   );
-}
+
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from "./redux/store";
+import { store } from './redux/store.ts';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+
+      <PersistGate persistor={persistor}>
+
+        <App />
+
+      </PersistGate>
+
+    </Provider>
   </StrictMode>,
 )
